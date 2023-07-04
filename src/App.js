a592929a3bee2fba6f8d3705dfcf751ec06376ea
uLinck <linck.developer@gmail.com>
2023-07-03 21:07:46 -0300

    push de uns treco ai

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this);
+
+    this.state ={
+      name: 'Linck',
+      counter: 1
+    };
+  }
+
+  handlePClick = event => {
+    console.log("meu deus");
+  }
+
+  handleAClick = (event) =>  {
+    event.preventDefault();
+    const {counter} = this.state
+    this.setState({
+      name: `heey, você apertou o gatinho ${counter} vezes!`,
+      counter: counter + 1
+    });
+    console.log("ta funcionando");
+  }
+
+  render() {
+    const { name } = this.state;
+
+      return (
     <div className="App">
       <header className="App-header">
         <p>oia o gatinho ai</p>
-        <img src='https://media.tenor.com/NFjEeHbk-zwAAAAC/cat.gif' className="catGif" alt="cat" />
+        <img 
+        src='https://media.tenor.com/NFjEeHbk-zwAAAAC/cat.gif'
+        alt="cat"
+        onClick={this.handleAClick} />
+
         <p>
-          testing <code>react-app</code>
+          {name}
         </p>
         <a 
           className="App-link"
@@ -20,6 +52,29 @@ function App() {
       </header>
     </div>
   );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>oia o gatinho ai</p>
+//         <img src='https://media.tenor.com/NFjEeHbk-zwAAAAC/cat.gif' className="catGif" alt="cat" />
+//         <p>
+//           testing <code>react-app</code>
+//         </p>
+//         <a 
+//           className="App-link"
+//           href="https://instagram.com/linckfx"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           meu insta p ver se funciona
+//         </a>
+//       </header>
+//     </div>
+//   );
+
 
 export default App;
